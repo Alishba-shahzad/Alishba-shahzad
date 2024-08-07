cd31a3d8c9148bba4a47743e6bc3bc4c536af122
Alishba-shahzad <alishbah722@gmail.com>
2024-08-07 06:41:29 -0700

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,3 @@
+console.log("Hello body in this world!");
+var user = { name: "Alish", age: 20 };
+console.log(user.name);
---
+++ intro.ts
@@ -0,0 +1,4 @@
+console.log("Hello body in this world!");
+
+let user = {name: "Alish" , age:20};
+console.log(user.name)
